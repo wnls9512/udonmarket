268ddfef475fda443146f101adb9fb91f1991dcd
vlovev <vlovev@DESKTOP-D8K2L0K>
2020-09-23 00:36:56 +0900

    BCryptPasswordEncoder added

--- udonmarket/src/main/java/com/kh/udon/member/controller/MemberController.java
+++ udonmarket/src/main/java/com/kh/udon/member/controller/MemberController.java
@@ -1,28 +1,58 @@
 package com.kh.udon.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.udon.member.model.service.MemberService;
+import com.kh.udon.member.model.vo.Member;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/member")
+@Slf4j
 public class MemberController
 {
     @Autowired
     private MemberService service;
     
+    @Autowired
+    private BCryptPasswordEncoder bcryptPasswordEncoder;
+    
+    
+    //로그인
     @RequestMapping("/loginForm")
     public String memberLogin()
     {
         return "member/memberLoginForm";
     }
     
+    // 회원가입
     @RequestMapping("/signupForm")
     public String memberSignup()
     {
         return "member/memberSignupForm";
+    }
+    
+    @RequestMapping(value="/signupForm", method=RequestMethod.POST)
+    public String memberSignup(Member member, RedirectAttributes redirectAttr) {
+    	
+    	
+    	log.debug("member = {} ", member);
+    	
+    	
+    	String rawPassword = member.getPassword();
+    	String encryptPassword = bcryptPasswordEncoder.encode(rawPassword);
+    	member.setPassword(encryptPassword);
+    	
+    	log.debug("rawPassword@Controller = {}", rawPassword);
+    	log.debug("encryptPassword@Controller = {}", encryptPassword);    	
+    	
+    	return "redirect:/";
     }
     
     @RequestMapping("/mypage")
